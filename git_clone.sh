a3c67dc4920814281b2fe1874293df47f1d7ad7c
Kyle Chesney <kyle.2493@gmail.com>
2019-06-26 09:45:15 -0400

    add karabiner config, spectacle config, git upstream script

---
+++ git_clone.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+args=("$@")
+
+upstream_org="ctsit"
+
+git clone "$1"
+echo "\nRepo cloned!\n"
+
+repo=$(echo ${1##*/}) # split by / and return final
+repo_dir=$(echo "$repo" | cut -d . -f 1)
+
+cd $repo_dir
+
+echo "Tracking develop"
+git checkout develop
+
+echo "Setting upstream"
+upstream_repo="git@github.com:${upstream_org}/${repo}"
+
+git remote add upstream "$upstream_repo"
+git remote -v
+git fetch --all
+
---
+++ macOS/README.md
@@ -0,0 +1,5 @@
+# Collection of scripts to run and configuration files to make macOS feel more like linux
+
+## Software to install
+[Karabiner-Elements](https://pqrs.org/osx/karabiner/index.html)  
+[Spectacle](https://www.spectacleapp.com/)
